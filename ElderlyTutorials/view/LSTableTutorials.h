642f679f249f9d8454bca56e4a069e4912296b48
kingSD <foxm5986@gmail.com>
2014-04-22 11:09:27 +0800

    view 2 update

---
+++ ElderlyTutorials/view/LSTableTutorials.h
@@ -0,0 +1,13 @@
+//
+//  LSTableTutorials.h
+//  ElderlyTutorials
+//
+//  Created by LeafShadow on 14-4-22.
+//  Copyright (c) 2014年 LeafShadows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSTableTutorials : UITableViewController
+
+@end
